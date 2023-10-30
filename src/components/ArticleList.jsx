ec600887c5e0b72c3d28b34d9519dad1c3c6fd73
Freja Fischer <frej657e@stud.kea.dk>
2023-10-30 13:48:03 +0100

    data drilling exercise 1

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,12 +1,12 @@
 import Article from "./Article";
-function ArticleList() {
+function ArticleList({ articles }) {
   return (
     <div>
       <h2>Vores artikler</h2>
       <div className="grid">
-        <Article />
-        <Article />
-        <Article />
+        <Article header={articles[0].header} content={articles[0].content} />
+        <Article header={articles[1].header} content={articles[1].content} />
+        <Article header={articles[2].header} content={articles[2].content} />
       </div>
     </div>
   );
