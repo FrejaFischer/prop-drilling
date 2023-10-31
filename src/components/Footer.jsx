8fd36c0eac7631cb701d4bffd57acd2f19bbdddf
Freja Fischer <frej657e@stud.kea.dk>
2023-10-31 08:34:26 +0100

    css modules working

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
+import style from "./Footer.module.css";
 function Footer(props) {
   return (
-    <footer>
+    <footer className={style.footer}>
       <p>Her er en meget standard footer hvor jeg gerne vil vise ({props.contactInfo})</p>
     </footer>
   );
